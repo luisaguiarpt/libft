1d00e784b60488dd51918cd58303bea1a3bff80c
luisaguiar <luisaguiar@duck.com>
2025-04-07 12:21:59 +0100

    tests for fncts that return int

--- libft_tests/tests/run_tests_1_1.sh
+++ libft_tests/tests/run_tests_1_1.sh
@@ -8,7 +8,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 # List of system function names
-SYS_FUNCS=("toupper" "tolower" "isprint" "isascii" "isalnum" "isdigit" "isalpha") 
+SYS_FUNCS=("isprint" "isascii" "isalnum" "isdigit" "isalpha") 
 
 for func in "${SYS_FUNCS[@]}"; do
 	ft_exec="bin/ft_$func"
@@ -20,21 +20,16 @@ for func in "${SYS_FUNCS[@]}"; do
 		rand_int=$(./bin/gen_rand_str 1)
 		arg="$rand_int"
 		output_sys=$($sys_exec "$arg")
-		echo "$output_sys\n"
+		output_sys=$(( output_sys ? 1 : 0))
 		output_ft=$($ft_exec "$arg")
-		echo "$output_ft\n"
 		input_display="\"$rand_int\""
 
-		# Convert numeric output to character for display
-		char_sys=$(printf \\$(printf '%03o' $output_sys))
-		char_ft=$(printf \\$(printf '%03o' $output_ft))
-
 		if [ "$output_sys" = "$output_ft" ]; then
-			echo -e "${GREEN}[PASS]${NC} $input_display -> $char_ft (sys: $char_sys)"
+			echo -e "${GREEN}[PASS]${NC} $input_display -> $output_ft (sys: $output_sys)"
 		else
 			echo -e "${RED}[FAIL]${NC} $input_display"
-			echo -e " Expected: '$char_sys' ($output_sys)"
-			echo -e " Got: '$char_ft' ($output_ft)"
+			echo -e " Expected: '$output_sys'"
+			echo -e " Got: '$output_ft'"
 		fi
 	done
 done
